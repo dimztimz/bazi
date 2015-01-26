dd09233cf6f9bc190b7027d02aec6563574ffe12
Dimitrij Mijoski <dmjpp@hotmail.com>
2015-01-27 00:21:12 +0100

    Staven teoretski proekt

--- paket_vidovi_prozivodi.sql
+++ paket_vidovi_prozivodi.sql
@@ -1,3 +1,6 @@
+--Zadaca so paketi po Napredni bazi na podatoci
+--Dimitrij Mijoski, 111132
+
 CREATE SEQUENCE SEQ_VIDOVI INCREMENT BY 1 MAXVALUE 9999999999999999999999999999 MINVALUE 1 CACHE 20;
 /
 
@@ -123,4 +126,110 @@ create or replace package body pak_vidovi as
 		commit;
 	end delete_vid;
 end pak_vidovi;
+/
+
+
+
+create sequence seq_proizvodi increment by 1 start with 7;
+/
+
+create or replace trigger tr_prozivodi_insert
+	before insert on proizvodi
+	for each row
+begin
+	if (:new.idProizvod is null or :new.idProizvod < 1) then
+		:new.idProizvod := seq_proizvodi.nextval;
+	end if;
+end;
+/
+
+select * from PROIZVODI;
+/
+
+create or replace package pak_proizvodi as
+	procedure insert_proizvod(p_ime varchar2, p_opis varchar2, p_idVid number,
+		p_idTip number, p_idNacinObrabotka number, p_datumDodavanje date);
+	procedure update_proizvod(p_id number, p_ime varchar2, p_opis varchar2,
+		p_idVid number, p_idTip number, p_idNacinObrabotka number,
+		p_datumDodavanje date);
+	procedure delete_proizvod(p_id number);
+	
+	function najdiBrojNaProizvodiSodrzatVid(p_vidId number) return number;
+end pak_proizvodi;
+/
+
+create or replace package body pak_proizvodi as
+	procedure insert_proizvod(p_ime varchar2, p_opis varchar2, p_idVid number,
+		p_idTip number, p_idNacinObrabotka number, p_datumDodavanje date) is
+		today date;
+		datum date;
+	begin
+		today := trunc(sysdate);
+		if (P_DATUMDODAVANJE is null or
+			P_DATUMDODAVANJE > today) then
+			datum := today;
+		else
+			datum := trunc(P_DATUMDODAVANJE);
+		end if;
+		
+		insert into proizvodi(IME, OPIS, VIDOVI_IDVID, TIP_PROIZVOD_IDTIPPROIZVOD,
+			NACIN_OBRABOTKA_IDOBRABOTKA, DATUMDODAVANJE)
+		values (p_ime, p_opis, p_idVid, p_idTip, p_idNacinObrabotka, p_datumDodavanje);
+		commit;
+	end;
+	
+	procedure update_proizvod(p_id number, p_ime varchar2, p_opis varchar2,
+		p_idVid number, p_idTip number, p_idNacinObrabotka number,
+		p_datumDodavanje date) is
+		red proizvodi%rowtype;
+	begin
+		select  idProizvod, nvl(p_ime, ime), nvl(p_opis, opis),
+		nvl(p_idVid, vidovi_idvid), nvl(p_idTip, TIP_PROIZVOD_IDTIPPROIZVOD),
+			nvl(p_idNacinObrabotka, nacin_obrabotka_idObrabotka),
+			nvl(p_datumDodavanje, datumDodavanje)
+		into red
+		from proizvodi
+		where idProizvod = p_id;
+		
+		update proizvodi
+		set ime=red.ime, opis=red.opis, vidovi_idvid=red.vidovi_idvid,
+			TIP_PROIZVOD_IDTIPPROIZVOD=red.TIP_PROIZVOD_IDTIPPROIZVOD,
+			nacin_obrabotka_idObrabotka=red.nacin_obrabotka_idObrabotka,
+			datumDodavanje=red.datumDodavanje
+		where idProizvod = p_id;
+		commit;
+	end;
+	
+	procedure delete_proizvod(p_id number) is
+	begin
+		delete from PROIZVODI
+		where PROIZVODI.IDPROIZVOD = P_ID;
+		commit;
+	end;
+	
+	function najdiBrojNaProizvodiSodrzatVid(p_vidId number) return number is
+		cursor c1 is
+		select idProizvod
+		from proizvodi
+		where vidovi_idVid = p_vidId;
+		idProizvodVar proizvodi.idProizvod%type;
+		brojac number(10) := 0;
+	begin
+		open c1;
+		loop
+			fetch c1 into idProizvodVar;
+			exit when c1%notfound;
+			brojac := brojac + 1;
+		end loop;
+		close c1;
+		
+		return brojac;
+	exception
+	when others then
+		if (c1%isopen) then
+			close c1;
+		end if;
+		return -1;
+	end;
+end pak_proizvodi;
 /
